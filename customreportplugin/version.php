52695264b7d944f7df731980641d5fe66285d1c5
DaniilKrotenko <83418644+DaniilKrotenko@users.noreply.github.com>
2023-08-24 16:03:05 +0300

    Update version.php

--- customreportplugin/version.php
+++ customreportplugin/version.php
@@ -1,4 +1,4 @@
 <?php
 $plugin->component = 'report_customreport';
-$plugin->version = 2023082300; // ГодМесяцДеньЧасМинута
-$plugin->requires = 2019111800; // Требуемая версия Moodle
+$plugin->version = 2023082300;
+$plugin->requires = 2019111800;
